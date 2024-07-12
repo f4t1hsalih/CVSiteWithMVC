be7b7791aec4b5cbb43c0202570ef7afcf8b330c
f4t1hsalih <f4t1hsalih@gmail.com>
2024-07-12 20:13:07 +0300

    Lecture 23 - Repository Design Pattern

---
+++ CVSiteWithMVC/Repositories/GenericRepository.cs
@@ -0,0 +1,36 @@
+﻿using CVSiteWithMVC.Models.Entity;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CVSiteWithMVC.Repositories
+{
+    public class GenericRepository<T> where T : class
+    {
+        CVSiteWithMVCEntities db = new CVSiteWithMVCEntities();
+
+        public void Insert(T t)
+        {
+            db.Set<T>().Add(t);
+            db.SaveChanges();
+        }
+        public void Update(T t)
+        {
+            db.SaveChanges();
+        }
+        public void Delete(T t)
+        {
+            db.Set<T>().Remove(t);
+            db.SaveChanges();
+        }
+        public List<T> GetListAll()
+        {
+            return db.Set<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return db.Set<T>().Find(id);
+        }
+
+    }
+}
