0a0fb57fdd4694079c8e7331216e505daa7a96d4
f4t1hsalih <f4t1hsalih@gmail.com>
2024-07-12 20:31:11 +0300

    Lecture 26 - Deneyimlerin Listelenmesi

---
+++ CVSiteWithMVC/Controllers/ExperienceController.cs
@@ -0,0 +1,17 @@
+﻿using CVSiteWithMVC.Repositories;
+using System.Web.Mvc;
+
+namespace CVSiteWithMVC.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        ExperienceRepository exp = new ExperienceRepository();
+
+        // GET: Experience
+        public ActionResult Index()
+        {
+            var values = exp.GetListAll();
+            return View(values);
+        }
+    }
+}
