b21e0cdb392f134e9628f9f56e09e9f1ecfa51b6
f4t1hsalih <f4t1hsalih@gmail.com>
2024-07-21 14:40:52 +0300

    Lecture 31 - Deneyim Güncelleme İşlemi

--- CVSiteWithMVC/Controllers/ExperienceController.cs
+++ CVSiteWithMVC/Controllers/ExperienceController.cs
@@ -11,7 +11,7 @@ namespace CVSiteWithMVC.Controllers
         // GET: Experience
         public ActionResult Index()
         {
-            var values = exp.GetListAll();
+            var values = exp.TGetListAll();
             return View(values);
         }
 
@@ -24,20 +24,31 @@ namespace CVSiteWithMVC.Controllers
         [HttpPost]
         public ActionResult AddExperience(tbl_experience p)
         {
-            exp.Insert(p);
+            exp.TInsert(p);
             return RedirectToAction("Index");
         }
         public ActionResult DeleteExperience(int id)
         {
-            var value = exp.GetById(id);
-            exp.Delete(value);
+            var value = exp.TGetById(id);
+            exp.TDelete(value);
             return RedirectToAction("Index");
         }
         [HttpGet]
         public ActionResult UpdateExperience(int id)
         {
-            var value = exp.GetById(id);
+            var value = exp.TGetById(id);
             return View(value);
+        }
+        [HttpPost]
+        public ActionResult UpdateExperience(tbl_experience p)
+        {
+            tbl_experience value = exp.TFind(x => x.exp_id == p.exp_id);
+            value.exp_title = p.exp_title;
+            value.exp_subtitle = p.exp_subtitle;
+            value.exp_description = p.exp_description;
+            value.exp_date = p.exp_date;
+            exp.TUpdate(value);
+            return RedirectToAction("Index");
         }
     }
 }
--- CVSiteWithMVC/Repositories/GenericRepository.cs
+++ CVSiteWithMVC/Repositories/GenericRepository.cs
@@ -1,6 +1,7 @@
 ﻿using CVSiteWithMVC.Models.Entity;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 
 namespace CVSiteWithMVC.Repositories
 {
@@ -8,28 +9,32 @@ namespace CVSiteWithMVC.Repositories
     {
         CVSiteWithMVCEntities db = new CVSiteWithMVCEntities();
 
-        public void Insert(T t)
+        public void TInsert(T t)
         {
             db.Set<T>().Add(t);
             db.SaveChanges();
         }
-        public void Update(T t)
+        public void TUpdate(T t)
         {
             db.SaveChanges();
         }
-        public void Delete(T t)
+        public void TDelete(T t)
         {
             db.Set<T>().Remove(t);
             db.SaveChanges();
         }
-        public List<T> GetListAll()
+        public List<T> TGetListAll()
         {
             return db.Set<T>().ToList();
         }
 
-        public T GetById(int id)
+        public T TGetById(int id)
         {
             return db.Set<T>().Find(id);
+        }
+        public T TFind(Expression<System.Func<T, bool>> where)
+        {
+            return db.Set<T>().FirstOrDefault(where);
         }
 
     }
