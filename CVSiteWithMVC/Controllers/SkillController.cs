2a0a67f6caea9e9acf1f63fc27c03bfb30484baf
f4t1hsalih <f4t1hsalih@gmail.com>
2024-08-21 20:34:10 +0300

    Lecture 33 - Yetenekleri Repository Üzerinden Listeleme

--- CVSiteWithMVC/Controllers/SkillController.cs
+++ CVSiteWithMVC/Controllers/SkillController.cs
@@ -1,17 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using CVSiteWithMVC.Models.Entity;
+using CVSiteWithMVC.Repositories;
 using System.Web.Mvc;
 
 namespace CVSiteWithMVC.Controllers
 {
     public class SkillController : Controller
     {
+        GenericRepository<tbl_skills> _skillsRepository = new GenericRepository<tbl_skills>();
         // GET: Skill
         public ActionResult Index()
         {
-            return View();
+            var skills = _skillsRepository.TGetListAll();
+            return View(skills);
+
         }
     }
 }
