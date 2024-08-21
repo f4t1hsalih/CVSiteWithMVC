17b296cfddcc16066436e7c47fcc031a2fa40fea
f4t1hsalih <f4t1hsalih@gmail.com>
2024-08-21 21:45:31 +0300

    Lecture 39 - Yeni Yetenek Ekleme

--- CVSiteWithMVC/Controllers/SkillController.cs
+++ CVSiteWithMVC/Controllers/SkillController.cs
@@ -7,12 +7,25 @@ namespace CVSiteWithMVC.Controllers
     public class SkillController : Controller
     {
         GenericRepository<tbl_skills> _skillsRepository = new GenericRepository<tbl_skills>();
+
         // GET: Skill
         public ActionResult Index()
         {
             var skills = _skillsRepository.TGetListAll();
             return View(skills);
+        }
 
+        // AddSkill
+        [HttpGet]
+        public ActionResult AddSkill()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddSkill(tbl_skills skill)
+        {
+            _skillsRepository.TInsert(skill);
+            return RedirectToAction("Index");
         }
     }
 }
