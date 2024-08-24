ea19a9326a99995f75bcb7e801383f86c0e96828
f4t1hsalih <f4t1hsalih@gmail.com>
2024-08-24 09:22:23 +0300

    Lecture 42 - Eğitim Listesi ve Eğitim Bilgisi Ekleme

---
+++ CVSiteWithMVC/Controllers/EducationController.cs
@@ -0,0 +1,33 @@
+﻿using CVSiteWithMVC.Models.Entity;
+using CVSiteWithMVC.Repositories;
+using System.Web.Mvc;
+
+namespace CVSiteWithMVC.Controllers
+{
+    public class EducationController : Controller
+    {
+
+        GenericRepository<tbl_education> educationRepo = new GenericRepository<tbl_education>();
+
+        // GET: Education
+        public ActionResult Index()
+        {
+            var values = educationRepo.TGetListAll();
+            return View(values);
+        }
+
+        // GET: Education/AddEducation
+        [HttpGet]
+        public ActionResult AddEducation()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddEducation(tbl_education p)
+        {
+            educationRepo.TInsert(p);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
