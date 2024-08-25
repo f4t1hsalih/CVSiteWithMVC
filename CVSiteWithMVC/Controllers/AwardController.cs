cd565c1eb15da84734b78dfb69241f6164b51fa6
f4t1hsalih <f4t1hsalih@gmail.com>
2024-08-25 13:05:45 +0300

    Lecture 48 - Sertifika Bilgilerinin Listelenmesi

--- CVSiteWithMVC/Controllers/AwardController.cs
+++ CVSiteWithMVC/Controllers/AwardController.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CVSiteWithMVC.Repositories;
+using CVSiteWithMVC.Models.Entity;
 
 namespace CVSiteWithMVC.Controllers
 {
     public class AwardController : Controller
     {
+
+        GenericRepository<tbl_award> awardRepo = new GenericRepository<tbl_award>();
+
         // GET: Award
         public ActionResult Index()
         {
-            return View();
+            var awards = awardRepo.TGetListAll();
+            return View(awards);
         }
     }
 }
