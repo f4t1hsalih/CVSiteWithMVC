791074bac99c9bb4a727b8670be5f45c1a565f15
f4t1hsalih <f4t1hsalih@gmail.com>
2024-08-26 14:24:01 +0300

    Lecture 54 - Hobi Düzenleme

--- CVSiteWithMVC/Controllers/InterestController.cs
+++ CVSiteWithMVC/Controllers/InterestController.cs
@@ -9,10 +9,19 @@ namespace CVSiteWithMVC.Controllers
         GenericRepository<tbl_interest> repo = new GenericRepository<tbl_interest>();
 
         // GET: Interest
+        [HttpGet]
         public ActionResult Index()
         {
             var interest = repo.TGetListAll();
             return View(interest);
+        }
+        [HttpPost]
+        public ActionResult Index(tbl_interest p)
+        {
+            var value = repo.TFind(x => x.int_id == 1);
+            value.int_interest = p.int_interest;
+            repo.TUpdate(value);
+            return RedirectToAction("Index");
         }
     }
 }
