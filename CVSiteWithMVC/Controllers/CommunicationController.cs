7e96b1554ae33f002def90c0d82cf816b28e2239
f4t1hsalih <f4t1hsalih@gmail.com>
2024-08-26 13:56:09 +0300

    Lecture 52 - İletişim Paneli

---
+++ CVSiteWithMVC/Controllers/CommunicationController.cs
@@ -0,0 +1,19 @@
+﻿using CVSiteWithMVC.Models.Entity;
+using CVSiteWithMVC.Repositories;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace CVSiteWithMVC.Controllers
+{
+    public class CommunicationController : Controller
+    {
+        GenericRepository<tbl_communication> repo = new GenericRepository<tbl_communication>();
+
+        public ActionResult Index()
+        {
+            var list = repo.TGetListAll();
+            return View(list);
+        }
+
+    }
+}
