4ec457edf2c7e194f854d1fb2d47546270136141
f4t1hsalih <f4t1hsalih@gmail.com>
2024-08-26 14:43:37 +0300

    Lecture 55 - Hakkımda Sayfası

--- CVSiteWithMVC/Controllers/AboutController.cs
+++ CVSiteWithMVC/Controllers/AboutController.cs
@@ -1,13 +1,36 @@
 ﻿using System.Web.Mvc;
+using CVSiteWithMVC.Models.Entity;
+using CVSiteWithMVC.Repositories;
 
 namespace CVSiteWithMVC.Controllers
 {
     public class AboutController : Controller
     {
+        GenericRepository<tbl_about> repo = new GenericRepository<tbl_about>();
         // GET: About
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            var about = repo.TGetListAll();
+            return View(about);
+        }
+        [HttpPost]
+        public ActionResult Index(tbl_about p)
+        {
+            var t = repo.TGetById(p.abt_id);
+            t.abt_name = p.abt_name;
+            t.abt_surname = p.abt_surname;
+            t.abt_address = p.abt_address;
+            t.abt_mail = p.abt_mail;
+            t.abt_tel = p.abt_tel;
+            t.abt_note = p.abt_note;
+            t.abt_photo = p.abt_photo;
+            t.abt_linkedin = p.abt_linkedin;
+            t.abt_github = p.abt_github;
+            t.abt_twitter = p.abt_twitter;
+            t.abt_instagram = p.abt_instagram;
+            repo.TUpdate(t);
+            return RedirectToAction("Index");
         }
     }
 }
