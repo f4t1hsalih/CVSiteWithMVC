8e736359fb484ced6c46fb7afe3391783d2b2cef
f4t1hsalih <f4t1hsalih@gmail.com>
2024-08-26 14:07:42 +0300

    Lecture 53 - Hobilerim Sayfası

---
+++ CVSiteWithMVC/Controllers/InterestController.cs
@@ -0,0 +1,18 @@
+﻿using CVSiteWithMVC.Models.Entity;
+using CVSiteWithMVC.Repositories;
+using System.Web.Mvc;
+
+namespace CVSiteWithMVC.Controllers
+{
+    public class InterestController : Controller
+    {
+        GenericRepository<tbl_interest> repo = new GenericRepository<tbl_interest>();
+
+        // GET: Interest
+        public ActionResult Index()
+        {
+            var interest = repo.TGetListAll();
+            return View(interest);
+        }
+    }
+}
